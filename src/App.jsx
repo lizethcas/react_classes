763e04cbe1244ca23f02d613dc56dfd33641261d
lizethcas <macbook@Macbooks-MacBook-Air.local>
2025-03-27 21:09:32 -0500

    renderizar usuarios, filtrar estado del usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,85 @@
 
-import { useState } from 'react'
+
 import { Section } from './components/Section'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css'
 
+
+const baseAvatar = "https://randomuser.me/api/portraits/women/"
+
+const initialUsers = [
+  { id: 1, name: "Laura", lastName: "Martínez", avatar: `${baseAvatar}/1.jpg`, active: true },
+  { id: 2, name: "Andrea", lastName: "Pérez", avatar: `${baseAvatar}/2.jpg`, active: false },
+  { id: 3, name: "Sofía", lastName: "Gómez", avatar: `${baseAvatar}/3.jpg`, active: true },
+  { id: 4, name: "Camila", lastName: "Rodríguez", avatar: `${baseAvatar}/4.jpg`, active: false },
+  { id: 5, name: "Mariana", lastName: "López", avatar: `${baseAvatar}/5.jpg`, active: true },
+  { id: 6, name: "Daniela", lastName: "Ramírez", avatar: `${baseAvatar}/6.jpg`, active: true },
+  { id: 7, name: "Fernanda", lastName: "Torres", avatar: `${baseAvatar}/7.jpg`, active: false },
+  { id: 8, name: "Paula", lastName: "Jiménez", avatar: `${baseAvatar}/8.jpg`, active: true },
+];
+
+const UserCard = ({ avatar, name, lastName, active }) => {
+
+
+  return (
+    <article>
+      <h2>{name} {lastName}</h2>
+      <img src={avatar} alt={`${name}-user`} />
+      <p>{active ? "activo " : "inactivo"}</p>
+    </article>
+  )
+
+}
+
+
 function App() {
-  const [state, setState] = useState(false)
-  const name = state ? "Andres" : "pepito"
+
+  const [users, setUsers] = useState([])
+  const [clear, setClean] = useState(false)
+
+  useEffect(() => {
+    // Código que quieres ejecutar como efecto secundario
+    console.log('Efecto ejecutado');
+    setUsers(initialUsers)
 
 
+  },[clear]);
 
 
-  const handeClick = () => {
+  const handleActive = () => {
+    const actives = users.filter(user => user.active)
+    setUsers(actives)
 
-    setState(!state)
+
   }
+  const handleInactive = () => {
+    const inactives = users.filter(user => !user.active)
+    setUsers(inactives)
+
+
+  }
+  const handleClear = () => {
+    console.log("aqui")
+    setClean(!clear)
+    
+
+
+  }
+
 
   return (
     <>
-      <Section name={name} >
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam tempora blanditiis, praesentium temporibus dolore necessitatibus consequatur commodi error, similique porro impedit magni maxime voluptatum pariatur! Aperiam ut nihil atque accusantium.</p>
-        <button onClick={handeClick}>Cambiar nombre</button>
-      </Section>
 
-      <Section name="liz" lastName="Castillo" />
+      {users.map(({ name, lastName, active, avatar, id }) => (
+        <UserCard name={name} lastName={lastName} active={active} avatar={avatar} key={id} />
 
+      ))
+      }
 
+      <button onClick={handleActive}>activos</button>
+      <button onClick={handleInactive}>inactivos</button>
+      <button onClick={handleClear}>limpiar</button>
     </>
 
 
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,13 @@
-export const Section = ({ name, lastName = "Desarrollo web", children }) => {
+
+export const Section = ({ name, lastName = "Desarrollo web", children, handeClick }) => {
+    
+   
+    
     return (
         <section className='title'>
             <h1>Hola {name} {lastName} !</h1>
             {children}
+            <button onClick={handeClick}>Cambiar nombre</button>
         </section>
     )
 }
